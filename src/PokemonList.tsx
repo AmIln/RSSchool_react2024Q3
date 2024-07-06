e8b5144701ed74bce57721af5b23138ea6ea9c1e
Dmitry Andriyanov <facklak@mail.ru>
2024-07-06 15:43:20 +0300

    feat: using class components for PokemonList

--- src/PokemonList.tsx
+++ src/PokemonList.tsx
@@ -1,44 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Result } from './interfaces/Result';
 
 const API_URL = 'https://pokeapi.co/api/v2/pokemon?limit=99&offset=0'; // ALL POKEMONS
 
-export const PokemonList = (): JSX.Element => {
-  const [pokemons, setPokemons] = useState<Result[]>([]);
+interface PokemonListState {
+  pokemons: Result[];
+}
 
-  useEffect(() => {
-    const fetchPokemons = async () => {
-      const result = await allPokemons();
-      if (result) {
-        setPokemons(result);
-      }
+export class PokemonList extends React.Component<object, PokemonListState> {
+  constructor(props: object) {
+    super(props);
+    this.state = {
+      pokemons: [],
     };
-
-    fetchPokemons();
-  }, []);
-
-  return (
-    <div className="pokemons-wrapper">
-      {pokemons.map((pokemon: Result) => (
-        <div key={pokemon.name} className="pokemon">
-          <h3 className="pokemon__name">{pokemon.name}</h3>
-          <p className="pokemon__url">{pokemon.url}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-const allPokemons = async (): Promise<Result[] | undefined> => {
-  try {
-    const response = await fetch(API_URL);
-    if (response.status === 404) {
-      return;
-    }
-
-    const data = await response.json();
-    return data.results;
-  } catch (err) {
-    console.log(err);
   }
-};
+
+  async componentDidMount(): Promise<void> {
+    const result = await this.allPokemons();
+    if (result) {
+      this.setState({ pokemons: result });
+    }
+  }
+
+  async allPokemons(): Promise<Result[] | undefined> {
+    try {
+      const response = await fetch(API_URL);
+      if (response.status === 404) {
+        return;
+      }
+
+      const data = await response.json();
+      return data.results;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  render(): JSX.Element {
+    return (
+      <div className="pokemons-wrapper">
+        {this.state.pokemons.map((pokemon: Result) => (
+          <div key={pokemon.name} className="pokemon">
+            <h3 className="pokemon__name">{pokemon.name}</h3>
+            <p className="pokemon__url">{pokemon.url}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default PokemonList;
